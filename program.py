e3b956b2b498a721c0c126f046fd92da003e5f74
Ryan <ryan.lolyork@gmail.com>
2023-02-06 22:13:46 -0500

    edit

--- program.py
+++ program.py
@@ -1,30 +1,38 @@
-from operator import add 
+from operator import add
 new_list = []
 while True:
-    inp = input('Enter the  exam and programming exercise results: ')
+    inp = input('Enter the exam and programming exercise results: ')
     if inp:
-        x, y = inp.split()
-        new_list.append(x)
-        new_list.append(y)
+      values = inp.split()
+      for elements in values:
+          if elements.isnumeric() == False:
+             values.clear()
+             print("Try Again")
+      if len(values) != 2:
+        print("Error: Invalid input, expected 2 arguments only")
+      else:
+       x, y = values
+       new_list.append(x)
+       new_list.append(y)
     else:
         print("----Statistics----")
-        
-        program_exercises = new_list[1::2] 
+
+        program_exercises = new_list[1::2]
         for items in range(0,len(program_exercises)):
-            program_exercises[items] = int(program_exercises[items]) 
-        program_points = []    
+            program_exercises[items] = int(program_exercises[items])
+        program_points = []
         for items in program_exercises:
             program_points.append(items/10)
-                           
-        
-        points = new_list[0::2] 
+
+
+        points = new_list[0::2]
         for items in range(0,len(points)):
-            points[items] = int(points[items]) 
-        
-        result = list(map(add, points, program_points))  
+            points[items] = int(points[items])
+
+        result = list(map(add, points, program_points))
         average_points = sum(result)/len(result)
         print(f"Points average : {average_points:.2f}")
-        
+
         counter = 0
         for items in result:
             if items <= 14:
@@ -33,7 +41,7 @@ while True:
             elif items >= 15:
                 percentage = ((len(result) - counter)/len(result)) * 100
         print(f"Pass percentage : {percentage:.2f}")
-        
+
         print("Grade distrubition:")
         fail_list = []
         grade_1 = []
@@ -59,9 +67,7 @@ while True:
                 grade_5.append(items)
             elif items > 30:
                 out_of_bounds.append(items)
-            
-                
-                
+
         print("0:" + len(fail_list) * "*")
         print("1:" + len(grade_1) * "*")
         print("2:" + len(grade_2) * "*")
@@ -70,6 +76,6 @@ while True:
         print("5:" + len(grade_5) * "*")
         print("OBB grade:" + len(out_of_bounds) * "*")
 
-                
+
         break
 input("Press enter to close the program. . . .")
